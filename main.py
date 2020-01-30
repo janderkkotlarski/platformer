4a3038f1fa411d2a9784713425142bf50c9ddd74
janderkkotlarski <j.d.kotlarski@gmx.com>
2020-01-30 22:00:50 +0100

    bouncy player

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
+import sys
+
 import pygame
 
+pygame.init()
+
+size = width, height = 1024, 768
+speed = [0, 0]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+player = pygame.image.load("player.png")
+player_rect = player.get_rect()
+
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    player_rect = player_rect.move(speed)
+
+    if player_rect.bottom > height:
+        speed[1] = -speed[1]
+
+    speed[1] = speed[1] + 1
+
+    screen.fill(black)
+    screen.blit(player, player_rect)
+    pygame.display.flip()

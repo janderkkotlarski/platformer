77e8ef0485ee51c6bea77902ec9809dec867dcb5
janderkkotlarski <j.d.kotlarski@gmx.com>
2020-02-06 21:59:00 +0100

    works nicely now

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import pygame
 pygame.init()
 
 
+def player_falls(player_rect, speed, height):
+    if player_rect.bottom >= height:
+        speed[1] = -speed[1]
+
+        if player_rect.bottom > height:
+            player_rect.bottom = height
+
+    speed[1] = speed[1] + gravity
+
+
 size = width, height = 800, 600
 speed = [0, 0]
 black = 0, 0, 0
@@ -21,21 +31,15 @@ player_rect.left = 0
 
 gravity = 0.05
 
-mix = 0.45
-
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-    gravitation(player_rect.bottom, speed[1], gravity, height)
+        # if event.type ==
 
-    speed[1] = speed[1] + mix * gravity
 
-    if player_rect.bottom >= height:
-        speed[1] = -speed[1]
-
-        if player_rect.bottom > height:
-           player_rect.bottom = height
+    player_falls(player_rect, speed, height)
 
     player_rect = player_rect.move(speed)
 
